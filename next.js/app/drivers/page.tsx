849850933d3654212b9328402d28da4f60c688eb
Marcus Marosvari <mdmarosvari@gmail.com>
2024-05-05 08:13:08 -0600

    Update drivers page

--- next.js/app/drivers/page.tsx
+++ next.js/app/drivers/page.tsx
@@ -1,12 +1,34 @@
 import { getDrivers } from "@/db/query";
+import { DriverSelect } from "@/db/types";
+import Image from "next/image";
+
+const Driver = ({ driver }: { driver: DriverSelect }) => {
+  return (
+    <div className="flex flex-row border p-4 rounded-lg">
+      <div className="flex-grow">
+        <h2 className="text-lg font-bold">{driver.nameFull}</h2>
+        <p>{driver.number}</p>
+        <p>{driver.countryCode}</p>
+      </div>
+      <div>
+        <Image
+          src={driver.headshotUrl}
+          alt={driver.nameFull}
+          width={80}
+          height={80}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default async function Drivers() {
   const drivers = await getDrivers();
   return (
-    <ul>
+    <div className="p-4 lg:72 xl:mx-96 flex flex-col gap-4">
       {drivers.map((driver) => (
-        <li key={driver.driverId}>{JSON.stringify(driver)}</li>
+        <Driver key={driver.driverId} driver={driver} />
       ))}
-    </ul>
+    </div>
   );
 }
